e0ff295270b30b84db5cb328ff4d41031dad439a
RicardoSMS <rsmartinez@bside.com.mx>
2022-01-20 21:36:26 -0600

    Set up the blob storage connection

--- Blob-Storage-Config/BlobConnection/Program.cs
+++ Blob-Storage-Config/BlobConnection/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace BlobConnection
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Azure blob strogae exercise");
+
+            // Corremos el ejemplo asincronicamente
+
+            ProcessAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Presiona una tecla para salir del ejercicio");
+            Console.ReadLine();
+        }
+
+        private static async Task ProcessAsync()
+        {
+            string storageConnectionString = "CONNECTION STRING";
+
+            //Crea un cliente que pueda autenticarce con la cadena de conexión
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+
         }
     }
 }
