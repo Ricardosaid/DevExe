0cbf7fddae6c0485a495667605ebefa858728158
RicardoSMS <rsmartinez@bside.com.mx>
2022-02-22 15:22:32 -0600

    Paso 2, agregamos mensajes al bucle
    creamos un mensaje e intentamos agregarlo al batch

--- ServiceBus/Program.cs
+++ ServiceBus/Program.cs
@@ -15,9 +15,36 @@ namespace ServiceBus
         // numero de mensajes que deben ser enviados a la cola
         private const int numOfMessages = 5;
 
-        
-        static void Main(string[] args)
+
+        static async task Main()
         {
+            // El service bus client usa un singleton, the best practice cuando publicamos o leemos mensajes
+            //Crea un ciente que se usará para enviar y procesar los mensajes
+            client = new ServiceBusClient (connectionString);
+            sender = client.CreateSender(queueName); //Sender es el obj que utilizartemos para enviar los msn a la queue
+
+            //Creamos el batch (lote de mensajes)
+
+            ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            // Agregamos los mensajes en un bucle
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                //trataremos de agregar los mensajes al lote, new ServiceBusMessage -> crea un nuevo objeto mensaje para enviar
+                //messageBatch.TryAddMessage -> intenta agregar un msn al lote, asegurando que el tamaño del lote no exceda su maximo, regresa un bool
+                if(!messageBatch.TryAddMessage(new ServiceBusMessage ($"Message {i}")))
+                {
+                    //si es muy largo el lote, creamos excepcion
+                    throw new Exception($"The message {i} es muy largo para ponerlo en el lote");
+                }
+            }
+
+
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
