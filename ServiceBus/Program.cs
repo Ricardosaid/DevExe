cc17306e73921a1def2c2e37240d4d6a5b7c72cb
RicardoSMS <rsmartinez@bside.com.mx>
2022-02-22 13:32:05 -0600

    Paso 1, declarar variables
    Declaro variables de conexion, client y sender

--- ServiceBus/Program.cs
+++ ServiceBus/Program.cs
@@ -4,6 +4,18 @@ namespace ServiceBus
 {
     class Program
     {
+        // Cadena de conexion para el namespace del service bus
+        static string connectionString = "";
+        // nombre de la cola del service bus
+        static string queueName = "";
+        // el cliente que posee la conexion y que puede ser usado para crear senders y receivers
+        static ServiceBusClient client;
+        //El sender encargado de publicar mensajes a la cola
+        static ServiceBusSender sender;
+        // numero de mensajes que deben ser enviados a la cola
+        private const int numOfMessages = 5;
+
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
