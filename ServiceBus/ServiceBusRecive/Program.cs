7f8537613f8c8405b53077310c00d193e1e7bb20
RicardoSMS <rsmartinez@bside.com.mx>
2022-02-22 19:04:51 -0600

    Paso 4, Processor in action
    Creamos el client y el processor, especificamos controladores, iniciamos el proceso y lo detuvimos

--- ServiceBus/ServiceBusRecive/Program.cs
+++ ServiceBus/ServiceBusRecive/Program.cs
@@ -1,23 +1,56 @@
-﻿using Sysmtem.Threading.Taks;
+﻿using System.Threading.Tasks;
 using System;
-using Azure.Messaging.Servicebus;
+using Azure.Messaging.ServiceBus;
 
 namespace ServiceBusRecive
 {
     class Program
     {
         //Es la cadena de conexion de Namespace de service Bus
-        static string connectionString = "";
+        static string connectionString = "Endpoint=sb://servicebusdemobfirst.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=41N/K1X1KnwoofRLbkqUOnkid/BGwwASITO1K8heJPs=";
         //Nombre de la cola del Service Bus
-        static string queueName = "";  
+        static string queueName = "demoqueue";  
         // El cliente que posee la conexion y puede ser usada para crear senders y recivers      
         static ServiceBusClient client;
         //El procesador que lee y procesa los mensajes que vienen de la queue
         static ServiceBusProcessor processor;
         
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("");
+            // creamos el objeto cliente del service bus
+            client =  new ServiceBusClient(connectionString);
+
+            //creamos el processor para manage los mensaje
+
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                // se agrega el controlador para procesar los mensajes
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // se agrega controlador para processar cualquier error
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                //Se empieza el procesamiento
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Espere un minuto y presione una tecla pata finalizar el proceso");
+                Console.ReadKey();
+
+                //Paramos el proceso
+                Console.WriteLine("\n Parando el reciver");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Reciver de mensajes detenido");
+
+            }
+            finally
+            {
+                // garantizar que los rexursos de red y otros objetos no administrados se limpien correctamente
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+                
+            }
         }
 
         //Controlador que recibe los mensajes
